2e919e2a78bab0455841d1e9718d448729df8329
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-19 09:33:53 -0300

    testing script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,6 +13,10 @@ npm install
 echo "Will build the app..."
 npm run build
 
+echo "Will commit..."
+git add -A
+git commit -m "deploy"
+
 echo "Will push the changes..."
 git push origin `git subtree split --prefix dist gh-pages`:gh-pages --force
 
