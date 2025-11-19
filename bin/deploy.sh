fc48c6f7fcd5f1636ef4a960f287041ad4fbd62f
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-19 08:31:57 -0300

    testing script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,10 +11,16 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+echo "Will configure user for github..."
+git config --global user.email "mariana.fantini@gmail.com"
+git config --global user.name "Mariana Fantini"
+
+echo "Will commit changes..."
 git init
 git add -A
 git commit -m 'deploy'
 
+echo "Will push changes..."
 git push -f https://marianafantini:$GITHUB_TOKEN@github.com:marianafantini/recipe-page-vuejs.git main:gh-pages
 
 cd -
