9ace4ed02ee14fa80a5054bb84cc4d1764098560
Mariana Fantini <mariana.fantini@gmail.com>
2025-11-19 09:31:18 -0300

    testing script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,7 +8,8 @@ git config --global user.email "mariana.fantini@gmail.com"
 git config --global user.name "Mariana Fantini"
 
 echo "Will build the app..."
-git subtree pull --prefix dist origin gh-pages
 npm run build
+
+echo "Will push the changes..."
 git push origin `git subtree split --prefix dist gh-pages`:gh-pages --force
 
